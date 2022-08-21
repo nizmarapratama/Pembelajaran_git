ce28aacdc062eba27cef46cdc71405e9113dcb69
nizmarapratama <nizmarapratama.@gmail.com>
2022-08-21 22:12:28 +0700

    punten akang

--- pembelajaran_pertama/src/App.js
+++ pembelajaran_pertama/src/App.js
@@ -7,7 +7,13 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Assalamualaikum Penonton
+        </p>
+        <p>
+          Nama saya Pulan
+        </p>
+        <p>
+          selamat menonton
         </p>
         <a
           className="App-link"
